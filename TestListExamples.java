3fc31423c6be34c3dcf208a9e09998e1386c0d94
austintnguyen <atn038@ucsd.edu>
2022-11-10 13:55:03 -0800

    Added a grading script

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,57 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
 
+class LongerThan3 implements StringChecker{
+
+  public boolean checkString(String s){
+    if(s.length()>3){
+      return true;
+    }
+    return false;
+  }
+}
 public class TestListExamples {
   // Write your grading tests here!
+
+@Test
+public void testFilter(){
+  ArrayList <String> list = new ArrayList<>();
+    list.add("apple");
+    list.add("a");
+    list.add("banana");
+    list.add("b");
+    ArrayList <String> expected = new ArrayList<>();
+    expected.add("apple");
+    expected.add("banana");
+    
+    StringChecker sc = new LongerThan3();
+    
+
+    assertEquals(expected, ListExamples.filter(list,sc));
+}
+
+@Test
+public void testMerge(){
+   ArrayList <String> list = new ArrayList<>();
+    ArrayList <String> list2 = new ArrayList<>();
+    list.add("apple");
+    list.add("b");
+    list.add("banana");
+    list2.add("cat");
+    list2.add("donkey");
+    list2.add("elephant");
+    list2.add("h");
+    ArrayList<String> expected = new ArrayList<>();
+    expected.add("apple");
+    expected.add("b");
+    expected.add("banana");
+    expected.add("cat");
+    expected.add("donkey");
+    expected.add("elephant");
+    expected.add("h");
+    
+    assertEquals(expected, ListExamples.merge(list,list2));
+}
+
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,58 @@
 # Create your grading script here
 
-set -e
+
 
 rm -rf student-submission
+
+echo "Removed student-submission directory"
 git clone $1 student-submission
+echo "cloned repository"
+
+cp TestListExamples.java student-submission
+
+
+echo "Copied Test file into student-submission"
+
+cp -r lib student-submission
+echo "Copied lib directory into student-submission"
+
+cd student-submission
+echo "Changed directory to student-submission"
+
+FILE=ListExamples.java
+if [ ! -f "$FILE" ];
+then
+    echo "$FILE was not found"
+    exit
+fi
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2>errorOutput.txt
+if [ -s errorOutput.txt ];
+then 
+echo "Compile Error, You failed!"
+exit
+fi
+
+echo "Sucessfully compiled tests"
+
+
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > goodOutput.txt
+
+echo "Sucessfully ran tests"
+
+
+
+if grep -q "2 tests" "goodOutput.txt" ;
+then
+echo "You passed. Congratulations!"
+exit
+fi
+
+cat goodOutput.txt
+echo "You Failed Buddy. Try again!"
+
+
+
+
 
